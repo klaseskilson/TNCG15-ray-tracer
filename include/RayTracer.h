a203e17a7e46c92d7b1ca2ac89f41678f96f592f
Klas Eskilson <klas.eskilson@gmail.com>
2016-09-01 11:44:41 +0200

    Add basic files

---
+++ include/RayTracer.h
@@ -0,0 +1,8 @@
+#ifndef TNCG15_RAYTRACER_H_H
+#define TNCG15_RAYTRACER_H_H
+
+class RayTracer {
+
+};
+
+#endif //TNCG15_RAYTRACER_H_H
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "RayTracer.h"
+
+int main() {
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
