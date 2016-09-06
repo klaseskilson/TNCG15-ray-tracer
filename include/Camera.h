dd176898d68b38291fafdaaec1b2faba6cde1974
Carl Englund <caren083@student.liu.se>
2016-09-06 12:34:38 +0200

    Implemented method to create a PPM-image. Its using C-style notation but it works so not sure we need to change it

--- include/Camera.h
+++ include/Camera.h
@@ -7,14 +7,18 @@
 
 class Camera {
 
-    public:
+public:
+        Camera() {};
+        ~Camera() {};
         void render();
         void createImage();
 
     private:
         //2D 1000x1000 array of type Pixel
         //Two instances of vertex one for each eye
-        //A bool to flip between the eys
+        //A bool to flip between the eye
+        static constexpr int WIDTH = 800;
+        static constexpr int HEIGHT = 800;
 
 };
 #endif //TNCG15_CAMERA_H
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,14 +1,38 @@
 //
 // Created by Carl Englund on 06/09/16.
 //
-
+#include <stdlib.h>
+#include <stdio.h>
 #include <Camera.h>
+#include <iostream>
 
 void Camera::render() {
 
 }
 
+/* How to write to a PPM-image from:
+ * https://rosettacode.org/wiki/Bitmap/Write_a_PPM_file#C
+ * */
 void Camera::createImage() {
+
+    std::cout << "Creating image...";
+
+    FILE *fp = fopen("rayTracedImage.ppm", "wb"); /* b - binary mode */
+
+    (void) fprintf(fp, "P6\n%d %d\n255\n", WIDTH, HEIGHT);
+
+    for (int j = 0; j < HEIGHT; ++j)
+    {
+        for (int i = 0; i < WIDTH; ++i)
+        {
+            static unsigned char color[3];
+            color[0] = i % 256;  /* red */
+            color[1] = j % 256;  /* green */
+            color[2] = (i * j) % 256;  /* blue */
+            (void) fwrite(color, 1, 3, fp);
+        }
+    }
+    (void) fclose(fp);
 
 }
 
