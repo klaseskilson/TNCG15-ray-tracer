6f8de1c14a9edfc58c9c57453c46edc8b5849592
Carl Englund <caren083@student.liu.se>
2016-09-06 15:41:22 +0200

    Added scene files

---
+++ include/Scene.h
@@ -0,0 +1,16 @@
+//
+// Created by Carl Englund on 06/09/16.
+//
+
+#ifndef TNCG15_SCENE_H
+#define TNCG15_SCENE_H
+
+class Scene {
+public:
+    //Should probably not be void..
+    void createScene();
+private:
+};
+
+
+#endif //TNCG15_SCENE_H
---
+++ src/Scene.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Carl Englund on 06/09/16.
+//
+
+#include "Scene.h"
+
+void Scene::createScene() {
+    //Create some triangles that form the room
+
+}
