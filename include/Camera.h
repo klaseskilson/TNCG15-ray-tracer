e63047822c455072e5034a261f1e81138108e5c9
Klas Eskilson <klas.eskilson@gmail.com>
2016-09-08 15:22:41 +0200

    Add member variables to Camera

--- include/Camera.h
+++ include/Camera.h
@@ -5,20 +5,25 @@
 #ifndef TNCG15_CAMERA_H
 #define TNCG15_CAMERA_H
 
+#include <array>
+#include "Pixel.h"
+
+const int WIDTH = 1000;
+const int HEIGHT = 1000;
+
 class Camera {
-
 public:
-        Camera() {};
-        ~Camera() {};
-        void render();
-        void createImage();
+    Camera() {};
+    ~Camera() {};
+    void render();
+    void createImage();
 
-    private:
-        //2D 1000x1000 array of type Pixel
-        //Two instances of vertex one for each eye
-        //A bool to flip between the eye
-        static constexpr int WIDTH = 800;
-        static constexpr int HEIGHT = 800;
+private:
+    //2D 1000x1000 array of type Pixel
+    //Two instances of vertex one for each eye
+    //A bool to flip between the eye
 
+    std::array<std::array<Pixel, HEIGHT>, WIDTH> pixels;
+    glm::vec3 position1, position2;
 };
 #endif //TNCG15_CAMERA_H
