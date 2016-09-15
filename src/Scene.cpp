6ff89235c5f0ee469277293502384e1ab1fc86ab
Carl Englund <caren083@student.liu.se>
2016-09-15 14:51:47 +0200

    Started working on room triangles..

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,7 +1,30 @@
 #include "Scene.h"
+#include "Triangle.h"
 
 void Scene::createScene() {
     //Create some triangles that form the room
+	//Floor
+	std::array<glm::vec3, 3> floor {glm::vec3(0.0f, 6.0f, -5.0f),glm::vec3(0.0f, -6.0f, -5.0f), glm::vec3(10.0f, -6.0f, -5.0f)};	
+	std::array<glm::vec3, 3> floor {glm::vec3(0.0f, 6.0f, -5.0f),glm::vec3(10.0f, -6.0f, -5.0f), glm::vec3(10.0f, 6.0f, 5.0f)};	
+	std::array<glm::vec3, 3> floor {glm::vec3(-3.0f, 0.0f, -5.0f), glm::vec3(0.0f, -6.0f, -5.0f), glm::vec3(0.0f, 6.0f, -5.0f)};	
+	std::array<glm::vec3, 3> floor {glm::vec3(10.0f, -6.0f, -5.0f), glm::vec3(13.0f, 0.0f, -5.0f), glm::vec3(10.0f, 6.0f, -5.0f)};	
+	//Right side back
+	std::array<glm::vec3, 3> rightSideBack {glm::vec3( 10, 0, 0),glm::vec3( 10, 0, 0), glm::vec3( 10, 0, 0)};	
+	//Left Side back
+	std::array<glm::vec3, 3> leftSideBack {glm::vec3( 10, 0, 0),glm::vec3( 10, 0, 0), glm::vec3( 10, 0, 0)};	
+	//Left Side front
+	std::array<glm::vec3, 3> leftSideFront {glm::vec3( 10, 0, 0),glm::vec3( 10, 0, 0), glm::vec3( 10, 0, 0)};	
+	//Right Side front
+	std::array<glm::vec3, 3> rightSideFront {glm::vec3( 10, 0, 0),glm::vec3( 10, 0, 0), glm::vec3( 10, 0, 0)};	
+	//Front
+	std::array<glm::vec3, 3> backSide {glm::vec3( 10, 0, 0),glm::vec3( 10, 0, 0), glm::vec3( 10, 0, 0)};	
+	//Back
+	std::array<glm::vec3, 3> frontSide {glm::vec3( 10, 0, 0),glm::vec3( 10, 0, 0), glm::vec3( 10, 0, 0)};	
+	//Floor
+	Triangle floorTriangles {floor};
+	//Roof
+	//Walls
+	
 }
 
 std::list<Triangle> Scene::detectIntersections(Ray ray) {
