d68ce57faa93cd47af82fcf045ba4cb02c00b506
Carl Englund <englund92@gmail.com>
2016-10-19 12:34:22 +0200

    Added light classes, not fully implemented

---
+++ include/Light.h
@@ -0,0 +1,13 @@
+//
+// Created by Carl Englund on 2016-10-19.
+//
+
+#ifndef TNCG15_LIGHT_H
+#define TNCG15_LIGHT_H
+class Light {
+public:
+    Light();
+private:
+    glm::vec3 position;
+};
+#endif //TNCG15_LIGHT_H
---
+++ src/Light.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Carl Englund on 2016-10-19.
+//
+
+Light::Light() {
+    position = glm::vec3(0.0f, 0.0f, 0.0f);
+}
