253a1b82fd93c6f56b55a921b9d281e2ba5653db
Carl Englund <englund92@gmail.com>
2016-10-21 09:27:08 +0200

    Added sphere class

--- include/Sphere.h
+++ include/Sphere.h
@@ -4,13 +4,22 @@
 
 #ifndef TNCG15_SPHERE_H
 #define TNCG15_SPHERE_H
-
+#include <glm/glm.hpp>
+#include "Ray.h"
 
 
 class Sphere {
+public:
+    Sphere(glm::vec3 v = glm::vec3(), float f = 0.0f, ColorDouble c = ColorDouble());
+    int sphereIntersection(Ray&, glm::vec3&);
+    const ColorDouble &getColor() const;
+
+private:
+    glm::vec3 position;
+    float radius;
+    ColorDouble color;
 
 };
-
 
 
 #endif //TNCG15_SPHERE_H
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,5 +1,49 @@
 //
 // Created by Carl Englund on 2016-10-20.
 //
-
 #include "Sphere.h"
+#define EPSILON 0.00000000000000001
+
+
+Sphere::Sphere(glm::vec3 mPosition, float mRadius, ColorDouble mColor) {
+    position = mPosition;
+    radius = mRadius;
+    color = mColor;
+}
+
+int Sphere::sphereIntersection(Ray& ray, glm::vec3 &intersection) {
+    //Ray offset from sphere center
+    glm::vec3 oc = position - ray.getStart();
+    float b = glm::dot(ray.getDirection()*2.0f, oc);
+    float d1 = -b/2.0f;
+    float d2 = d1;
+    float delta = std::pow(d1, 2) - (glm::dot(oc, oc) - std::pow(radius, 2));
+
+    if(delta < -EPSILON) {
+        return false;
+    }
+    else if( delta > -EPSILON && delta < EPSILON) {
+        intersection = ray.getStart()+d1*ray.getDirection();
+        return true;
+    }
+
+    float dSqrt = std::sqrt(delta);
+
+    d1 -= dSqrt;
+    d2 += dSqrt;
+
+    if(d1 > EPSILON) {
+        intersection = ray.getStart()+d1*ray.getDirection();
+        return true;
+    }
+    else if(d2 > EPSILON) {
+        intersection = ray.getStart()+d2*ray.getDirection();
+        return true;
+    }
+
+    return false;
+}
+
+const ColorDouble &Sphere::getColor() const {
+    return color;
+}
