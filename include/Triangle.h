214136babe45a584432695624a36abb79cd55b14
Carl Englund <englund92@gmail.com>
2016-10-21 09:27:32 +0200

    Forgot to add header file from removing sphereIntersection in triangle

--- include/Triangle.h
+++ include/Triangle.h
@@ -17,19 +17,14 @@ public:
     Triangle(const std::array<glm::vec3, 3> p) : Triangle(p[0], p[1], p[2], color) {};
     Triangle(const glm::vec3&, const glm::vec3&, const glm::vec3&, const ColorDouble &color = ColorDouble(0.0f));
     int intersection(Ray&, glm::vec3&);
-    int sphereIntersection(Ray&, glm::vec3&);
-
     glm::vec3 calcNormal() { return glm::normalize(glm::cross(edge1(), edge2())); }
-
     const glm::vec3 &getNormal() const;
-
     const ColorDouble &getColor() const;
 
 private:
     std::array<glm::vec3, 3> positions;
     ColorDouble color;
     glm::vec3 normal;
-
     glm::vec3 edge1() { return positions[1] - positions[0]; }
     glm::vec3 edge2() { return positions[2] - positions[0]; }
 };
