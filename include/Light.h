cb55e0b24d8f640a002c19e8a049d29758365516
Carl Englund <englund92@gmail.com>
2016-10-25 16:59:11 +0200

    Updated light class so we can add a area light

--- include/Light.h
+++ include/Light.h
@@ -6,11 +6,17 @@
 #define TNCG15_LIGHT_H
 
 #include <glm/vec3.hpp>
+#include <vector>
+#include "definitions.h"
+#include "Triangle.h"
 
 class Light {
 public:
     Light();
+    const std::vector<Triangle> &getTriangles() const;
 private:
-    glm::vec3 position;
+    std::vector<Triangle> triangles;
+    ColorDouble color;
+
 };
 #endif //TNCG15_LIGHT_H
--- src/Light.cpp
+++ src/Light.cpp
@@ -6,5 +6,17 @@
 #include <Light.h>
 
 Light::Light() {
-    position = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    vec3 topLeft (-1.0f, 5.0f, 8.0f);
+    vec3 topRight(1.0f, 5.0f, 8.0f);
+    vec3 bottomRight (-1.0f, 5.0f, 6.0f);
+    vec3 bottomLeft (1.0f, 5.0f, 6.0f);
+
+    const Surface White(ColorDouble(1.0f), 2);
+    triangles.push_back(Triangle(bottomRight, topRight, bottomLeft, White));
+    triangles.push_back(Triangle(bottomRight, bottomLeft, topLeft, White));
+}
+
+const std::vector<Triangle> &Light::getTriangles() const {
+    return triangles;
 }
