b242fbe69d8d9e08fdc4c05ec0b0428e33156554
Klas Eskilson <klas.eskilson@gmail.com>
2016-10-25 09:38:00 +0200

    Add RNG with min & max

--- include/utilities.h
+++ include/utilities.h
@@ -8,11 +8,9 @@ static std::default_random_engine GENERATOR;
 static std::uniform_real_distribution<double> DISTR(0.0, 1.0);
 
 double uniformRand();
+double randMinMax(const double &min, const double &max);
+
 std::ostream &operator<< (std::ostream &out, const vec3 &vec);
 
-//double randMinMax(double min, double max) {
-//    std::uniform_real_distribution<double> dist(min, max);
-//    return dist(GENERATOR);
-//}
 
 #endif //TNCG15_UTILS_H
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -4,6 +4,10 @@ double uniformRand() {
     return DISTR(GENERATOR);
 }
 
+double randMinMax(const double &min, const double &max) {
+    return min + uniformRand() * (max - min);
+}
+
 std::ostream &operator<< (std::ostream &out, const vec3 &vec) {
     out << "{"
         << vec.x << " " << vec.y << " "<< vec.z
